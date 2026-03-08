85aae7ddb33997e38dce7163573919ba88a70080
hggg11 <4311063@alumni.tus.ac.jp>
2026-03-08 21:21:36 +0900

    docker,postgresqlを導入しデータの永続化

--- todo-backend/src/main/java/com/example/todobackend/config/SecurityConfig.java
+++ todo-backend/src/main/java/com/example/todobackend/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.todobackend.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()  // ログインは認証不要
